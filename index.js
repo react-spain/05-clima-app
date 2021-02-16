20a68dab4eeedf4be31fa7d41b2624e31f89bd73
Marlom <falconsoft.3d@gmail.com>
2021-02-16 18:17:56 +0100

    67

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const { leerInput } = require("./helpers/inquirer")
+const { leerInput, inquirerMenu, pausa } = require("./helpers/inquirer")
 
 const main = async() =>{
     let opt;
-}
+    do{ 
+        opt = await inquirerMenu();
+        console.log({ opt })
+        if ( opt !== 0 ) await pausa();
+    } while ( opt !== 0 )
 
+}
 main();
